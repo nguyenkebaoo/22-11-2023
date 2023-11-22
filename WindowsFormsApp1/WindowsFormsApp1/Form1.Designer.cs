b80071f4d4b28e86deb7258709883039dc3e1c34
Nguyễn Kế Bảo <baonguyen301103@gmail.com>
2023-11-22 16:00:44 +0700

    Ngay22_11_2023

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioBtnOption1 = new System.Windows.Forms.RadioButton();
+            this.radioBtnOption2 = new System.Windows.Forms.RadioButton();
+            this.radioBtnOption3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(148, 77);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(46, 17);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "label1";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioBtnOption1
+            // 
+            this.radioBtnOption1.AutoSize = true;
+            this.radioBtnOption1.Location = new System.Drawing.Point(84, 157);
+            this.radioBtnOption1.Name = "radioBtnOption1";
+            this.radioBtnOption1.Size = new System.Drawing.Size(110, 21);
+            this.radioBtnOption1.TabIndex = 6;
+            this.radioBtnOption1.TabStop = true;
+            this.radioBtnOption1.Text = "radioButton1";
+            this.radioBtnOption1.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnOption2
+            // 
+            this.radioBtnOption2.AutoSize = true;
+            this.radioBtnOption2.Location = new System.Drawing.Point(270, 157);
+            this.radioBtnOption2.Name = "radioBtnOption2";
+            this.radioBtnOption2.Size = new System.Drawing.Size(110, 21);
+            this.radioBtnOption2.TabIndex = 7;
+            this.radioBtnOption2.TabStop = true;
+            this.radioBtnOption2.Text = "radioButton2";
+            this.radioBtnOption2.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnOption3
+            // 
+            this.radioBtnOption3.AutoSize = true;
+            this.radioBtnOption3.Location = new System.Drawing.Point(447, 157);
+            this.radioBtnOption3.Name = "radioBtnOption3";
+            this.radioBtnOption3.Size = new System.Drawing.Size(110, 21);
+            this.radioBtnOption3.TabIndex = 8;
+            this.radioBtnOption3.TabStop = true;
+            this.radioBtnOption3.Text = "radioButton3";
+            this.radioBtnOption3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 378);
+            this.Controls.Add(this.radioBtnOption3);
+            this.Controls.Add(this.radioBtnOption2);
+            this.Controls.Add(this.radioBtnOption1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioBtnOption1;
+        private System.Windows.Forms.RadioButton radioBtnOption2;
+        private System.Windows.Forms.RadioButton radioBtnOption3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeQuestions();
+            ShowQuestion();
+        }
+        private void InitializeQuestions()
+        {
+            // Tạo danh sách các câu hỏi
+            questions = new List<Question>
+            {
+                new Question("Câu hỏi 1: Bạn học môn kĩ năng mềm nào chưa?", new List<string> { "Chưa", "Đã học", "Học hết rồi" }, 0),
+                new Question("Câu hỏi 2: Nguyễn Huệ và Quang Trung có mối quan hệ gì?", new List<string> { "Anh em", "Cha con", "Một người" }, 1),
+                new Question("Câu hỏi 3: Trời đang mưa hay nắng?", new List<string> { "Mưa", "Nắng", "Mưa nắng" }, 2),
+            };
+
+            // Khởi tạo chỉ số câu hỏi hiện tại
+            currentQuestionIndex = 0;
+        }
+        private void ShowQuestion()
+        {
+            // Hiển thị câu hỏi và các đáp án
+            Question currentQuestion = questions[currentQuestionIndex];
+            lblQuestion.Text = currentQuestion.Content;
+
+            radioBtnOption1.Text = currentQuestion.Options[0];
+            radioBtnOption2.Text = currentQuestion.Options[1];
+            radioBtnOption3.Text = currentQuestion.Options[2];
+
+            // Xóa chọn đáp án
+            radioBtnOption1.Checked = false;
+            radioBtnOption2.Checked = false;
+            radioBtnOption3.Checked = false;
+        }
+            private void button1_Click(object sender, EventArgs e)
+        {
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.Count)
+            {
+                ShowQuestion();
+            }
+            else
+            {
+                MessageBox.Show("Kết thúc đề thi!");
+                this.Close(); // Đóng ứng dụng sau khi kết thúc
+            }
+        }
+        public class Question
+        {
+            public string Content { get; }
+            public List<string> Options { get; }
+            public int CorrectOptionIndex { get; }
+
+            public Question(string content, List<string> options, int correctOptionIndex)
+            {
+                Content = content;
+                Options = options;
+                CorrectOptionIndex = correctOptionIndex;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
